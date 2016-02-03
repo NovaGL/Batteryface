786fcbd03ec3704a8a4e559b5d9c28b87a39a140
NovaGL <gl1@live.com>
2016-02-03 14:56:11 +1100

    Support Pebble Round

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <pebble.h>
 #define KEY_DATE_FORMAT 0
-#define KEY_BT_FORMAT 0
+#define KEY_BT_FORMAT 1
+#define KEY_VIBE_ON_DISCONNECT 2
+#define KEY_VIBE_ON_CONNECT 3
+#define LOW_POWER 50
+
 
 static Window *s_main_window;
 static TextLayer *s_time_layer, *s_date_layer;
@@ -10,39 +14,54 @@ static GFont s_time_font, s_date_font;
 static BitmapLayer *s_background_layer, *s_bt_icon_layer;
 static GBitmap *s_background_bitmap, *s_bt_icon_bitmap;
 
-static int s_battery_level;
+static int s_battery_level, parent_bounds;
 
-static bool euro_date = 1;
 static bool bt_toggle = 0;
+static bool euro_date = 1;
+static bool charge = 0;
+static bool vibe_on_disconnect = 0;
+static bool vibe_on_connect = 0;
+
+
+// Grab settings
+enum {
+  CONFIG_dateformat = 0x0,
+  CONFIG_bt = 0x1, 
+  CONFIG_bt_disconnect = 0x2,
+  CONFIG_bt_connect = 0x3
+};
 
 // Record Bluetooth connection state
 static void bluetooth_callback(bool connected) {
 	
-  if (bt_toggle == 1) {
+  if (bt_toggle == 1) {   
+      APP_LOG(APP_LOG_LEVEL_INFO, "Hide Bluetooth connection symbol"); 
       if		(!connected) {bitmap_layer_set_bitmap(s_bt_icon_layer, gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BT_0));}
-      else	{bitmap_layer_set_bitmap(s_bt_icon_layer,gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BLANK));}
-			//vibes_long_pulse();
-			//}
+      else	{bitmap_layer_set_bitmap(s_bt_icon_layer,gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BLANK_WHITE));}			    			
   } else {
+      APP_LOG(APP_LOG_LEVEL_INFO, "Show Bluetooth connection symbol"); 
       if		(!connected) {bitmap_layer_set_bitmap(s_bt_icon_layer, gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BT_0));}
 	    else	{bitmap_layer_set_bitmap(s_bt_icon_layer,gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BT_1));}			    
   }
 
-}
-
-enum {
-  CONFIG_dateformat = 0x0,
-  CONFIG_bt = 0x1  
-};
-
-static void battery_callback(BatteryChargeState state) {
-  // Record the new battery level
-  s_battery_level = state.charge_percent;
+  // set vibrations
+  if (!connected) {		
+		if (vibe_on_disconnect == 1) {
+      APP_LOG(APP_LOG_LEVEL_INFO, "Vibrate on Bluetooth disconnect"); 
+			vibes_long_pulse();
+		}
+	} else {
+		if (vibe_on_connect == 1) {
+      APP_LOG(APP_LOG_LEVEL_INFO, "Vibrate on Bluetooth reconnection"); 
+			vibes_double_pulse();
+		}
+	}
   
-  // Update meter
-  layer_mark_dirty(s_battery_layer);
 }
 
+
+
+////////////////// Time and Date Status//////////////////////
 static void update_time() {
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
@@ -64,12 +83,6 @@ static void update_time() {
   static char datn_buffer[] = "DD"; // Buffer for date number
   strftime(datn_buffer, sizeof("DD"), "%d", tick_time); // Write current date to buffer
   
- /* if (*dateformat = "1") {
-      strftime(date_buffer, sizeof(date_buffer), "%a %b %d", tick_time);
-  } else {
-      strftime(date_buffer, sizeof(date_buffer), "%a %d %b", tick_time);
-    }*/
-  
   if (euro_date == 1) { // If the user has selected the WWW DD MMM date format
 		strftime(date_buffer, sizeof(date_buffer), "%a %d %b", tick_time);
 	} else {
@@ -80,20 +93,45 @@ static void update_time() {
 }
 //////////////////////////////// Battery Level Graphics ///////////////////////////////////
 static void battery_update_proc(Layer *layer, GContext *ctx) {
-  GRect bounds = layer_get_bounds(layer);
+   
+ GRect bounds = layer_get_bounds(layer);
   
   // Find the width of the bar
-  int width = (int)(float)(((float)s_battery_level / 100.0F) * 114.0F);
+  int width = (int)(float)(((float)s_battery_level / 100.0F) * parent_bounds);
   
   // Draw the background
   graphics_context_set_fill_color(ctx, GColorDarkGray);
   graphics_fill_rect(ctx, bounds, GCornerNone, 0);
   
-  // Draw the bar
+  // Draw the battery bar  
+  
   graphics_context_set_fill_color(ctx, GColorMediumSpringGreen);
-  graphics_fill_rect(ctx, GRect(0, 0, width, bounds.size.h), GCornerNone, 0);
+     
+  
+  if (s_battery_level <= 20 && charge == 0)  {
+      graphics_context_set_fill_color(ctx, GColorRed);      
+  }  
+  
+   if (charge == 1)  {
+       graphics_context_set_fill_color(ctx, GColorIcterine);    
+  }  
+   graphics_fill_rect(ctx, GRect(0, 0, width, bounds.size.h), GCornerNone, 0);
+ 
 }
-
+//////////////////////////////// Battery Charge Graphics ///////////////////////////////////
+static void battery_callback(BatteryChargeState state) {
+  // Record the new battery level
+  s_battery_level = state.charge_percent;
+  
+    if (state.is_plugged) {
+      charge = 1;
+    } else {
+      charge = 0;
+    }  
+    
+  // Update meter
+  layer_mark_dirty(s_battery_layer);
+}
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
@@ -102,35 +140,40 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void main_window_load(Window *window) {
   
   window = window_create();
-	window_stack_push(window, true);
+  window_stack_push(window, true);
 	window_set_background_color(window, GColorBlack);
-	
   
+	Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  parent_bounds = bounds.size.w -20;
+    
   // Create GFonts
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_46));
   s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
    
     
-  // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(5, 35, 139, 50));
+  // Create TIME TextLayer
+  s_time_layer = text_layer_create(GRect(0, 40, bounds.size.w, bounds.size.h));
+  //s_time_layer = text_layer_create(GRect(5, 40, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorVividCerulean);
   text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   
-  // Create date TextLayer
-  s_date_layer = text_layer_create(GRect(0, 115, 144, 30));
-  text_layer_set_text_color(s_date_layer, GColorWhite);
+  
+  // Create DATE TextLayer
+  s_date_layer = text_layer_create(GRect(0, 115, bounds.size.w, bounds.size.h));
+  text_layer_set_text_color(s_date_layer, GColorFromHEX(0xFFFFFF));
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
   text_layer_set_text(s_date_layer, "Sept 23");
   text_layer_set_font(s_date_layer, s_date_font);
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
 
-  // Create battery meter Layer
-  s_battery_layer = layer_create(GRect(17, 100, 115, 2));
+  // Create BATTERY meter Layer
+  s_battery_layer = layer_create(GRect(10, 102, bounds.size.w -20, 2));
   layer_set_update_proc(s_battery_layer, battery_update_proc);
   layer_add_child(window_get_root_layer(window), s_battery_layer);
   
@@ -184,17 +227,25 @@ static void deinit() {
   window_destroy(s_main_window);
 }
 
-
+/////////  Recieved Settings from JS ////////////////////
 void in_received_handler(DictionaryIterator *received, void *context) {
+  
+  bt_toggle = 0;
+  vibe_on_connect = 0;
+  vibe_on_disconnect = 0;
+  
   // incoming message received
   Tuple *date_tuple = dict_find(received, CONFIG_dateformat);
   Tuple *bt_tuple = dict_find(received, CONFIG_bt);
+  Tuple *vibe_on_connect_t = dict_find(received, CONFIG_bt_connect); // int8
+  Tuple *vibe_on_disconnect_t = dict_find(received, CONFIG_bt_disconnect); // int8
+    
   
    if (date_tuple) {
   	APP_LOG(APP_LOG_LEVEL_INFO, "KEY_DATE_FORMAT received!");
 
   	if (strcmp(date_tuple->value->cstring, "edate") == 0) {
-  		APP_LOG(APP_LOG_LEVEL_INFO, "Using european date");
+  		APP_LOG(APP_LOG_LEVEL_INFO, "Using European date");
   		euro_date = 1;
   		persist_write_int(KEY_DATE_FORMAT, euro_date);
   	} else {
@@ -205,8 +256,7 @@ void in_received_handler(DictionaryIterator *received, void *context) {
    }
   
   if (bt_tuple){
-      if (strcmp(bt_tuple->value->cstring, "on") == 0) {
-        APP_LOG(APP_LOG_LEVEL_INFO, "Hide Bluetooth connection symbol"); 
+      if (strcmp(bt_tuple->value->cstring, "on") == 0) {        
         bt_toggle = 1;
         persist_write_int(KEY_BT_FORMAT, bt_toggle);
       } else {
@@ -215,6 +265,17 @@ void in_received_handler(DictionaryIterator *received, void *context) {
         persist_write_int(KEY_BT_FORMAT, bt_toggle);
       }
     }  
+  
+   if (vibe_on_connect_t) {
+  	APP_LOG(APP_LOG_LEVEL_INFO, "KEY_VIBE_ON_CONNECT received!");
+  	vibe_on_connect = vibe_on_connect_t->value->int8;
+  }
+
+  if (vibe_on_disconnect_t) {
+  	APP_LOG(APP_LOG_LEVEL_INFO, "KEY_VIBE_ON_DISCONNECT received!");
+  	vibe_on_disconnect = vibe_on_disconnect_t->value->int8;
+  }
+  
   update_time();
   bluetooth_callback(connection_service_peek_pebble_app_connection());
 }
